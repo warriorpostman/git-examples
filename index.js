b7209b5af34378b536dbd48d728c3d5ad1d61e79
Rustam Kasad <warriorpostman@gmail.com>
2016-09-15 09:25:55 -0700

    Add test route.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/test', function (req, res) {
+  res.send('A new test route!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });

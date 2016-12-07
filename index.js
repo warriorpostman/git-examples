12f8898e37431176929d3fb13da28830143f64d6
Rustam Kasad <warriorpostman@gmail.com>
2016-12-07 15:00:22 -0800

    Revert "Add a 'wacky' route."
    This reverts commit c74ba1efcb84494d6838f08d3029aa487d0404b3.

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ app.get('/some-crazy-route', function (req, res) {
   res.send('This route is crazy');
 });
 
-app.get('/wacky-route', function (req, res) {
-  res.send('This is a wacky route.');
-});
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });

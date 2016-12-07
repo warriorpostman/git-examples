6d55ffc4a404b527019c7b9f625f15978af92aea
Rustam Kasad <warriorpostman@gmail.com>
2016-12-07 14:38:55 -0800

    Use JWT to secure HTTP route. Starting to get the hang of this.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var jwt = require('express-jwt');
+
 var service = require('./services/get-data');
 
 app.get('/data', function (req, res) {
@@ -10,13 +12,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/secure', function (req, res) {
-  // TODO: 
+app.get('/secure', 
+  jwt({ 
+    secret: 'BESURETODRINKYOUROVALTINE' 
+  }),
+  function (req, res) {
+  // TODO:
   // Hmm, look for something that indicates
   // a client should be allowed to use this service, 
   // I think ?
   res.send('Secure: Hello World!');
-});
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');

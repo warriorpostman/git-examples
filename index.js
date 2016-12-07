f7c62b0e83109b60dd523f678d2c52f158bb2dda
Rustam Kasad <warriorpostman@gmail.com>
2016-12-07 14:29:32 -0800

    Add a route that will need secure access.

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/secure', function (req, res) {
+  // TODO: 
+  // Hmm, look for something that indicates
+  // a client should be allowed to use this service, 
+  // I think ?
+  res.send('Secure: Hello World!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });

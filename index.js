875df0f1a74be986057524855d024925bccf0f36
Rustam Kasad <warriorpostman@gmail.com>
2016-12-07 14:45:05 -0800

    Add a 'crazy' route.

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/some-crazy-route', function (req, res) {
+  response.send('This route is crazy');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
